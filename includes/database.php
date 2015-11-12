4a419675a2ed17d88f0bd2a9a7f32858da5d9efc
Kyle Geske <stungeye@gmail.com>
2015-11-12 15:01:19 -0600

    Reformatting tabs in database include file.

--- includes/database.php
+++ includes/database.php
@@ -1,34 +1,36 @@
 <?php
-    class Database {
-        
-      const HOST = 'localhost';
-      const DBNAME = 'boardgamegeek';
-      const DBUSER = 'serveruser';
-      const DBPASSWORD = 'gorgonzola7!';
-      
-      private $connection;
-      
-      public static function get_instance() {
+
+class Database {
+    const HOST = 'localhost';
+    const DBNAME = 'boardgamegeek';
+    const DBUSER = 'serveruser';
+    const DBPASSWORD = 'gorgonzola7!';
+
+    private $connection;
+
+    public static function get_instance() {
         static $instance = null;
+
         if (null === $instance) {
-          $instance = new static();
+            $instance = new static();
         }
         return $instance;
-      }
-      
-      private function __construct() {
-        try {
-          $this->connection = new PDO("mysql:host=".self::HOST.";dbname=".self::DBNAME, self::DBUSER, self::DBPASSWORD);
-        } catch(PDOException $e) {
-          die("DB: " . $e->getMessage());
-        }
-      }
-      
-      public static function prepare_and_execute($sql, $data) {
-          $connection = self::get_instance()->connection;
-          $prepared = $connection->prepare($sql);
-          $prepared->execute($data);
-          return $prepared;
-      }
     }
+
+    private function __construct() {
+        try {
+            $this->connection = new PDO("mysql:host=".self::HOST.";dbname=".self::DBNAME, self::DBUSER, self::DBPASSWORD);
+        } catch(PDOException $e) {
+            die("DB: " . $e->getMessage());
+        }
+    }
+
+    public static function prepare_and_execute($sql, $data) {
+        $connection = self::get_instance()->connection;
+        $prepared = $connection->prepare($sql);
+        $prepared->execute($data);
+        return $prepared;
+    }
+}
+
 ?>
