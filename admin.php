a292587fbaa4d1ee271bd153b64f555b081479a3
Kyle Geske <stungeye@gmail.com>
2015-11-16 14:24:48 -0600

    Starting work on the admin CRUD for games.

---
+++ admin.php
@@ -0,0 +1,25 @@
+<?php
+    require('includes\includes.php');
+    
+    $games = find_all_games();
+
+    require('partials\header.php');
+?>
+
+<h1>Bored Game Geek Admin</h1>
+
+<p><a href="new.php">Add New Game</a></p>
+
+<h2>Edit Existing Games</h2>
+
+<?php if (count($games) == 0): ?>
+    <p>No games present in system.</p>
+<?php else: ?>
+    <ul>
+        <?php foreach($games as $game): ?>
+            <li><a href="edit.php?id=<?= $game['id'] ?>"><?= $game['name'] ?></a></li>
+        <?php endforeach ?>
+    </ul>
+<?php endif ?>
+
+<?php require('partials\footer.php'); ?>
