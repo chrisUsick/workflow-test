edd772690080f088461a2f6e71f26eeaaf433673
Chris Usick <christopher.usick@gmail.com>
2017-02-20 22:48:43 -0600

    added a footer

--- web/partials/footer.php
+++ web/partials/footer.php
@@ -1,4 +1,5 @@
 </div> <!-- div.container -->
+    <footer>Repo: <a href="https://github.com/chrisUsick/workflow-test>chrisUsick/workflow-test</a></footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
     <script src="js/summernote.min.js"></script>
